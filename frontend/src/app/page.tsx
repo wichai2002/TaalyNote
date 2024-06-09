ba0a1e649df4a4dd7428a6e99691956c5f918190
kommongkhun wichai <kommongkhun.wichai@gmail.com>
2024-06-09 14:02:55 +0700

    new frontend

--- frontend/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-
-function HomePage() {
-
-  return <div>Welcome to Next.js!</div>
-
-}
-
-export default HomePage
--- frontend/tallry/src/app/layout.tsx
+++ frontend/tallry/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../styles/globals.css";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,12 +16,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-       <head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Jacques+Francois&display=swap"
-            rel="stylesheet"
-          />
-        </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
